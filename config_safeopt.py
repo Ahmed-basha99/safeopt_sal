d8bc045c6c59a345a1c7a73a0cf0adb334a5ad86
ahm13597 <ahmed.bahssain2.iosb-ina@fraunhofer.de>
2025-10-28 20:10:07 +0100

    trying constant hyperparam

--- config_safeopt.py
+++ config_safeopt.py
@@ -3,7 +3,7 @@ import math
 
 #  D set 
 
-N_POINTS = 50
+N_POINTS = 200
 DOMAIN = torch.linspace(0, 1, N_POINTS).view(-1, 1) 
 
 def ground_truth(x):
@@ -12,13 +12,13 @@ def ground_truth(x):
 # h
 SAFETY_THRESHOLD = -0.2
 
-INITIAL_SAFE_INDICES = torch.tensor([ 2, 4,50]) 
+INITIAL_SAFE_INDICES = torch.tensor([ 1,2,3]) 
 INITIAL_X = DOMAIN[INITIAL_SAFE_INDICES]
 INITIAL_Y = ground_truth(INITIAL_X).flatten()
 
-LIPSCHITZ_CONSTANT =2*math.pi # max derivative 
+LIPSCHITZ_CONSTANT = 2*math.pi # max derivative 
 
-BETA = 2.0
+BETA = 4.0
 
 # domain discritaztion 
 
