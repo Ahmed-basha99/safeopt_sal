34f562161dff9e09a05ffeae1d02545b635df285
ahmed-basha99 <ahmed.bahusain@gmail.com>
2025-11-25 11:42:34 +0100

    minor updates

--- beta.py
+++ beta.py
@@ -8,11 +8,12 @@ def gamma_bound_srinivas09 (t,D_size,sigma,kxx) :
 def beta_srinivas09(t,D_size,sigma,kxx, delta = 0.1, B = 0) : 
     return 2*B + 300 * (math.log(t/delta))**3 * gamma_bound_srinivas09(t,D_size,sigma,kxx) 
 
-def beta_fiedler21( R, lieklihood_noise,K,delta = 0.05 ,B=0) :
+def beta_fiedler21(R, lieklihood_noise,K,delta = 0.05 ,B=0) :
     K = K.detach().numpy()
     lam_bar = max(1,lieklihood_noise)
     inside_det = K + lam_bar * np.eye(K.shape[0]) 
     inside_log = np.linalg.det(inside_det)
     inside_sqr = math.log(inside_log)-2*math.log(delta)
+    print(B,R,math.sqrt(inside_sqr))
     return B+R*math.sqrt(inside_sqr)
 
